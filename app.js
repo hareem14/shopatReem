c35360bdb7fe51a7c4d0d1ea26231f18cdca5da9
hareem14 <hareemfatima_14@hotmail.com>
2020-06-06 23:30:00 +0500

    https://github.com/hareem14/shopatReem.git

---
+++ app.js
@@ -0,0 +1,44 @@
+
+  jQuery(function ($) {
+    $('#rating1').shieldRating({
+        max: 5,
+        step: 1,
+        value: 3,
+        markPreset: false
+    });
+
+    $('#rating2').shieldRating({
+        max: 5,
+        step: 1,
+        value: 4,
+        markPreset: false
+    });
+    $('#rating3').shieldRating({
+        max: 5,
+        step: 1,
+        value: 2,
+        markPreset: false
+    });
+    $('#rating4').shieldRating({
+        max: 5,
+        step: 1,
+        value: 3,
+        markPreset: false
+    });
+
+    $('#rating5').shieldRating({
+        max: 5,
+        step: 1,
+        value: 4,
+        markPreset: false
+    });
+    $('#rating6').shieldRating({
+        max: 5,
+        step: 1,
+        value: 2,
+        markPreset: false
+    });
+});
+
+
+
